8b68c1900895cebd207608299458026be64c7472
JoseDiegoSD <josediegosd13@gmail.com>
2025-01-22 12:39:31 -0700

    terminal

---
+++ js/functions.js
@@ -0,0 +1,34 @@
+// Declaracion de variables globales
+var contador = 0;
+var queen = "./img/queen.png"
+
+function mostrarReina(celda) {
+  if (window.getComputedStyle(celda).backgroundImage=='none') {
+    if (contador < 8) {
+    
+      celda.style=`
+              background-image: url(${queen});
+              background-repeat: no-repeat;
+              background-position: center;`;
+              contador++;
+    }
+  } else {
+    celda.style=`
+              background-image: none;`;
+              contador--;
+  }
+}
+
+function cambiarColor(r, c) { /* Renglones Columnas*/
+  var celda = document.getElementById("tablero");
+  var r1 = r, c1 = c, r2 = r, c2 = c;
+  var r3 = r, c3 = c, r4 = r, c4 = c;
+  for (let i = 0; i < 8; i++) { /* Let para variables locales (dentro de una funcion) */
+    celda.rows[r].cells[i].style.backgroundColor="red";
+    celda.rows[i].cells[c].style.backgroundColor="red";
+  }
+}
+
+function limpiar(){
+    document.querySelectorAll("td").forEach(td => td.style.backgroundColor = "");
+}
